a84ca42d60a754857134ea04d34085af540486e2
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-25 16:57:40 -0700

    Creating constant.py file and adding logic

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+
+# File path for our Data Lake Parquet export 
+parquet_path = "weather_staging.parquet"
+
+# Target API coordinates (Latitude and Longitude)
+cities = {
+
+    "New York": {"lat": 40.7128, "long": -74.0060},
+    "London": {"lat": 51.5074, "long": -0.1278},
+    "Tokyo": {"lat": 35.6762, "long": 139.6503},
+    "Sao_Paulo": {"lat": -23.5505, "long": -46.6333}
+
+}
