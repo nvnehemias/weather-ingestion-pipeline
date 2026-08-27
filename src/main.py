de68da19fe6a60ff2fb356bcdc7a84b678b7ab38
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-26 17:15:54 -0700

    Finish writing code for main

--- src/main.py
+++ src/main.py
@@ -9,3 +9,22 @@ def run_pipeline():
     raw_data = []
 
     # Step 1: Extract
+    for city, coords in cities.items():
+        print(f"Fetching data for {city}")
+        record = extract_weather(city, coords["lat"],coords["lon"])
+        raw_data.append(record)
+
+    # Step 2: Transform
+    df_clean = transform_data(raw_data)
+
+    # Step 3: Data Quality Assertion
+    validate_quality(df_clean)
+
+    # Step 4: Load
+    load_data(df_clean)
+
+    print("Pipeline finished cleanly!")
+
+
+if __name__ == "__main__":
+    run_pipeline()
